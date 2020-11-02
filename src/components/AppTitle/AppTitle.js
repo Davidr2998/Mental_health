87bc9e08ff4d8e9a7c75c53c834467197d65305d
tmchein <davidr2998@gmail.com>
2020-11-02 18:29:08 -0500

    Cambios realizados en el componente de app title

--- src/components/AppTitle/AppTitle.js
+++ src/components/AppTitle/AppTitle.js
@@ -1,10 +1,11 @@
 import React from "react";
+import "./AppTitle.css";
 
 const AppTitle = (props) => {
   const { name } = props;
   return (
     <div className="o-app-title-container">
-      <h2>{name}</h2>
+      <h2 className="o-app-title">{name}</h2>
     </div>
   );
 };
