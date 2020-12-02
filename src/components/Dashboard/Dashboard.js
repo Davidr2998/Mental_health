bdd3db2e3e133dc4c29df5f3dafd736de203f5e7
joseDmultimedia <jose_dav.garcia@uao.edu.co>
2020-12-01 22:36:28 -0500

    Este es el modal, falta arreglar su estilo y adaptarlo a citas

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React , { useState } from "react";
 import Navbar from "../MainPage/Navbar/Navbar";
 import "./Dashboard.css";
 import OptionContainer from "./OptionContainer/OptionContainer";
 import WelcomeMessage from "./WelcomeMessage/WelcomeMessage";
+import { Modal } from '../Modal/Modal.js';
+
+
 
 const Dashboard = () => {
+
+  const [show, setShow] = useState(false);
+  const closeModalHandler = () => setShow(false);
+
   return (
+    
     <div className="o-dashboard-container">
       <Navbar isLoggedIn={true} username="Juan David Rojas" />
       <WelcomeMessage username="Juan David Rojas" />
       <OptionContainer />
+
+      { show ? <div onClick={closeModalHandler} className="back-drop"></div> : null }
+      <button onClick={() => setShow(true) } className="btn-openModal" >Abrir Modal</button>
+      <Modal show={show} close={closeModalHandler} />
     </div>
   );
 };
---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Modal.css';
+
+export const Modal = ({ show, close }) => {
+  return (
+    <div className="modal-wrapper" 
+      style={{
+        transform: show ? 'translateY(0vh)' : 'translateY(-100vh)',
+        opacity: show ? '1' : '0'
+      }}
+    >
+      <div className="modal-header">
+        <p>Titulo del Moddal</p>
+        <span onClick={close} className="close-modal-btn">x</span>
+      </div>
+      <div className="modal-content">
+        <div className="modal-body">
+          <h4>Modal</h4>
+          <p>Un texto</p>
+        </div>
+        <div className="modal-footer">
+          <button onClick={close} className="btn-cancel">Close</button>
+        </div>
+      </div>
+    </div>
+  )
+};
